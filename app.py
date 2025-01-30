672e08584823aa589fab8d94f583a708e50a8d8b
Hugo Arroyo <hugoarroyogalle@gmail.com>
2025-01-30 16:15:14 +0100

    Hacemos funcional y aplicamos estilos a la interfaz de registro

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from dotenv import load_dotenv
 from flask_mysqldb import MySQL
 from os import getenv
 
-app = Flask(__name__)
+load_dotenv()
 
+app = Flask(__name__)
+mysql = MySQL(app)
 app.config['MYSQL_HOST'] = getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = getenv('MYSQL_DB')
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 
 
 
@@ -17,8 +20,34 @@ def raiz():
     return render_template('index.html')
 
 
-@app.route('/register')
+@app.route('/register', methods = ['GET', 'POST'])
 def register():
+    cursor = mysql.connection.cursor()
+    cursor.execute('use flask_login_contacts_app')
+    cursor.execute('select * from usuarios')
+    usuarios = cursor.fetchall()
+    correos = []
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        correo = request.form['correo']
+        clave = request.form['clave']
+        
+        if nombre != '' and correo != '' and clave != '':
+
+            for usuario in usuarios:
+                correos.append(usuario[2])
+                
+
+
+            if correo in correos:
+                flash('El usuario ya existe', 'error')
+            else:
+                correos.append(correo)
+                cursor.execute('INSERT INTO usuarios(nombre, correo, clave) VALUES (%s, %s, %s)', (nombre, correo, clave))
+                mysql.connection.commit()
+                flash('Usuario añadido correctamente', 'success')
+        else:
+            flash('Debes rellenar todos los campos')
     return render_template('register.html')
 
 @app.route('/login')
---
+++ database/flask-login-contacts-app.sql
@@ -0,0 +1,7 @@
+-- create database if not exists flask_login_contacts_app;
+/* create table if not exists usuarios (
+    id int not null auto_increment primary key,
+    nombre varchar(50) not null,
+    correo varchar(100) not null,
+    clave varchar(255) not null
+);*/
